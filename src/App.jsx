8ca9748c447caaf8c4eb83e5fcadda3275b9a0b6
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-06-26 23:07:21 -0600

    Cambiando forma de importar el App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-function App() {
+import { useState } from "react";
+
+export function App() {
+  const [newTaskName, setNewTaskName] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem("tasks", newTaskName);
+    setNewTaskName("");
+  };
+
   return (
     <div className="App">
-      <input type="text" placeholder="Ingresa nueva tarea" />
-      <button onClick={() => alert("click")}>Save Task</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ingresa nueva tarea"
+          value={newTaskName}
+          onChange={(event) => setNewTaskName(event.target.value)}
+        />
+        <button>Save Task</button>
+      </form>
     </div>
   );
 }
-
-export default App;
