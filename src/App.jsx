6ec47e78838b66314c3a51067e808087d5aee72e
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-06-26 22:42:29 -0600

    Creando Proyect

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <input type="text" placeholder="Ingresa nueva tarea" />
+      <button onClick={() => alert("click")}>Save Task</button>
+    </div>
+  );
+}
+
+export default App;
