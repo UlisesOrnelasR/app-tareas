da08bb482e2e5da6db42bc5b70a4c6c6c8459fff
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-06-26 23:17:57 -0600

    Importando nuevo componente <TaskCreator.jsx />

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import { useState } from "react";
+import { TaskCreator } from "./components/TaskCreator.jsx";
 
 export function App() {
-  const [newTaskName, setNewTaskName] = useState();
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    localStorage.setItem("tasks", newTaskName);
-    setNewTaskName("");
-  };
-
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Ingresa nueva tarea"
-          value={newTaskName}
-          onChange={(event) => setNewTaskName(event.target.value)}
-        />
-        <button>Save Task</button>
-      </form>
+      <TaskCreator />
     </div>
   );
 }
