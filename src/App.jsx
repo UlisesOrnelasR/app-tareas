6c62deb9a6071cf01e32991f543f80476d791232
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-06-27 18:14:43 -0600

    Iniciando Lista de Tareas

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,40 @@ import { useState } from "react";
 import { TaskCreator } from "./components/TaskCreator.jsx";
 
 export function App() {
+  const [taskItems, setTaskItems] = useState([
+    {
+      name: "mi primer tarea",
+      done: false,
+    },
+    {
+      name: "mi segunda tarea",
+      done: false,
+    },
+    ,
+    {
+      name: "mi tercer tarea",
+      done: false,
+    },
+  ]);
+
   return (
     <div className="App">
       <TaskCreator />
+
+      <table>
+        <thead>
+          <tr>
+            <th>Tasks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {taskItems.map((task) => (
+            <tr key={task.name}>
+              <td>{task.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
